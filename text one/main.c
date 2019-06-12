348ece6d1cf1b64a765a1f51a0acc1d5f4b61463
s20181106277 <s20181106277@czm.local>
2019-06-12 20:24:14 +0800

    Initial Commit

---
+++ text one/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  text one
+//
+//  Created by s20181106277 on 2019/6/12.
+//  Copyright © 2019 s20181106277. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
